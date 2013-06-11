020e91fdfed28801b19f037f8c11a7e71d282608
Brandon Tomlinson <thebwt@gmail.com>
2013-06-11 11:58:51 -0500

    Removing all that werkzeug stuff for now, may use it more later

--- app/models.py
+++ app/models.py
@@ -1,38 +1,9 @@
 from app import collection
 
-from werkzeug.exceptions import NotFound
-
 Users = collection["users"]
 
 ROLE_USER = 0
 ROLE_ADMIN = 1
-
-def getUserByNick(userid):
-    spec = {
-        "email": userid
-    }
-    return Users.find_one(spec)
-
-class Resource(object):
-
-    def GET(self, request, **kwargs):
-        return NotImplemented()
-
-    def HEAD(self, request, **kwargs):
-        return NotImplemented()
-
-    def POST(self, request, **kwargs):
-        return NotImplemented()
-
-    def DELETE(self, request, **kwargs):
-        return NotImplemented()
-
-    def PUT(self, request, **kwargs):
-        return NotImplemented()
-
-    def __call__(self, request, **kwargs):
-        handler = getattr(self, request.method)
-        return handler(request, **kwargs)
 
 class User(Resource):
     
@@ -60,22 +31,6 @@ class User(Resource):
     def __setitem__(self, key, value):
         self.doc[key]=value
         
-    def GET(self, request, userid):
-        doc = getUserByNick(userid)
-        if not doc:
-            return NotFound
-        return doc
-        
-        
-    def PUT(self, request, userid):
-        doc = getUserByNick(userid)
-        if not doc:
-            return NotFound
-        return doc
-        
-    def POST(self, request, userid):
-        pass
-    
     def __repr__(self):
         return '<User %r>' % (self.nickname)
         
