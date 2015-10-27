19c3bce113e3f91626cb8ac3433f09d8a7aa77d0
carlgeorge <carl.george@rackspace.com>
2015-10-27 15:47:52 -0500

    pep8 fixes

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,8 @@ from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 
 from config import basedir
+from app import views
+
 
 collection = Connection()["test-site"]
 
@@ -19,5 +21,3 @@ lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 oid = OpenID(app, os.path.join(basedir, 'tmp'))
-
-from app import views
--- app/views.py
+++ app/views.py
@@ -9,24 +9,29 @@ from forms import LoginForm, PostForm
 
 from models import ROLE_USER, ROLE_ADMIN, User
 
-@app.route('/', methods = ['GET', 'POST'])
-@app.route('/index', methods = ['GET', 'POST'])
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     user = g.user
     form = PostForm()
     if form.validate_on_submit():
-        collection['posts'].insert({'body':form.body.data, 'author':g.user.doc['_id'], 'time': datetime.utcnow().isoformat()})
+        payload = {'body': form.body.data,
+                   'author': g.user.doc['_id'],
+                   'time': datetime.utcnow().isoformat()}
+        collection['posts'].insert(payload)
     posts = collection['posts'].find().sort('time', -1).limit(5)
-    return render_template("index.html",        
-        title = 'Home',        
-        user = user,        
-        posts = posts,
-        form = form,
-        col = collection
-        )
-        
+    return render_template(
+        "index.html",
+        title='Home',
+        user=user,
+        posts=posts,
+        form=form,
+        col=collection
+    )
 
-@app.route('/login', methods = ['GET', 'POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 @oid.loginhandler
 def login():
     if g.user is not None and g.user.is_authenticated():
@@ -34,38 +39,43 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         session['remember_me'] = form.remember_me.data
-        return oid.try_login(form.openid.data, ask_for = ['nickname', 'email'])
-    return render_template('login.html', 
-        title = 'Sign In',
-        form = form,
-        providers = app.config['OPENID_PROVIDERS'])
-        
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
+    return render_template(
+        'login.html',
+        title='Sign In',
+        form=form,
+        providers=app.config['OPENID_PROVIDERS']
+    )
+
+
 @lm.user_loader
 def load_user(id):
-    u = collection['users'].find_one({'email':id})
-    collection['log'].insert({'id':id, 'user':u})
+    u = collection['users'].find_one({'email': id})
+    collection['log'].insert({'id': id, 'user': u})
     if u:
         return User(id)
     else:
         return None
-    
+
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
-    
+
+
 @oid.after_login
 def afer_login(resp):
     if resp.email is None or resp.email == "":
         flash('Invalid login, please try again.')
         return redirect(url_for('login'))
-    
-    user = collection['users'].find_one({'email':resp.email})
+
+    user = collection['users'].find_one({'email': resp.email})
     if user is None:
         nickname = resp.nickname
         if nickname is None or nickname == "":
             nickname = resp.email.split('@')[0]
-        user = {'nickname':nickname, 'email':resp.email, 'role':ROLE_USER}
+        user = {'nickname': nickname, 'email': resp.email, 'role': ROLE_USER}
         collection['users'].insert(user)
         user = User(resp.email)
     else:
@@ -73,11 +83,10 @@ def afer_login(resp):
     remember_me = False
     if 'remember_me' in session:
         remember_me = session['remember_me']
-    login_user(user, remember = remember_me )
+    login_user(user, remember=remember_me)
     return redirect(request.args.get('next') or url_for('index'))
-    
+
+
 @app.before_request
 def before_request():
     g.user = current_user
-
-    
